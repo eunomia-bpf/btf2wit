195d797e9b85768b282dd33aa307f7c1f7e3df88
officeyutong <yt.xyxx@gmail.com>
2023-02-08 16:43:34 +0000

    add license

--- src/btf_types/mod.rs
+++ src/btf_types/mod.rs
@@ -1,3 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
+ *
+ * Copyright (c) 2023, eunomia-bpf
+ * All rights reserved.
+ */
+
 use anyhow::anyhow;
 use btf::types::{Btf, BtfType};
 use std::collections::HashMap;
