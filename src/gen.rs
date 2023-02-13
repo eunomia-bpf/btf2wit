643cac469a25f9bde55c5bee9cfa3ca6c326f734
officeyutong <yt.xyxx@gmail.com>
2023-02-13 12:52:11 +0000

    add: Support for manually specifying pointer size

--- src/gen.rs
+++ src/gen.rs
@@ -4,20 +4,24 @@
  * All rights reserved.
  */
 
-use crate::btf_types::BtfUtils;
+use crate::btf_types::{BtfUtils, GenerateArgs};
 use btf::types::{Btf, BtfType};
 use std::io::Write;
 
-pub fn generate_wit<'a>(btf: &Btf<'a>) -> anyhow::Result<Vec<u8>> {
+pub fn generate_wit<'a>(btf: &Btf<'a>, args: GenerateArgs) -> anyhow::Result<Vec<u8>> {
     let mut out_buf = Vec::<u8>::new();
-    let mut btf_util = BtfUtils::new(&btf);
+    let mut btf_util = BtfUtils::new(&btf, args);
     let types = btf.types();
 
     writeln!(out_buf, "default world host {{")?;
 
     for (idx, ty) in types.iter().enumerate() {
         match ty {
-            BtfType::Struct(_) | BtfType::Union(_) | BtfType::Enum(_) | BtfType::Func(_) | BtfType::Typedef(_) => write!(
+            BtfType::Struct(_)
+            | BtfType::Union(_)
+            | BtfType::Enum(_)
+            | BtfType::Func(_)
+            | BtfType::Typedef(_) => write!(
                 out_buf,
                 "{}",
                 btf_util.generate_top_level_string(idx as u32, 4)?
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -7,14 +7,14 @@
 use btf::types::Btf;
 use object::ElfFile;
 
-use crate::gen::generate_wit;
+use crate::{btf_types::GenerateArgs, gen::generate_wit};
 
 #[test]
 fn test_nested_array() {
     let elf_bytes = include_bytes!("nested-array.bpf.o");
     let elf: ElfFile = object::ElfFile::parse(elf_bytes).unwrap();
     let btf = Btf::load(&elf).unwrap();
-    let out = generate_wit(&btf).unwrap();
+    let out = generate_wit(&btf, GenerateArgs::default().pointer_size(64)).unwrap();
     let expected_out = include_bytes!("nested-array-out.txt");
     assert_eq!(out, expected_out);
 }
